8a12b2a4c96562f42e32eb791665e1d00d47c933
Huypt8x <youremail@example.com>
2025-09-23 00:17:58 +0700

    chore: update vercel.json & vite.config.mjs for production deploy

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -2,5 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: './',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+  },
+  server: {
+    port: 3000,
+    open: true
+  }
 })
